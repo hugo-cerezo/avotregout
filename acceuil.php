560fd152db9b30bc512d8b1f815e091ed912f364
hugo-cerezo <hugo.cerezo-baelo@laplateforme.io>
2020-03-24 08:33:15 +0100

    v

--- acceuil.php
+++ acceuil.php
@@ -2,7 +2,7 @@
       
     <aricle>
       <div id='acceuil1'>
-        
+        <p>Particulier<p>
       </div>
     </aricle>   
     <aricle>
@@ -20,11 +20,36 @@
             <p>galerie photo</p>
           </div>
     </aricle>
+    <aricle>
+    <div id='acceuilFacebook'>
+            <p>add fb api here</p>
+          </div>
+    </aricle>
+    <aricle>
+    <div id='acceuilAdmin'>
+            <p>News from admin</p>
+          </div>
+    </aricle>
+    <aricle>
+    <div id='acceuilCommande'>
+            <h2>Vous souhaitez passer une commande ?</h2>
+            <h3>BLABLA lien vers page devis</h3>
+          </div>
+    </aricle>
+    <aricle>
+    <div id='acceuilWho'>
+            <h2>Bio</h2>
+            <h3>Francisco cerezo baelo aka Paco</h3>
+            <p>Restaurateur vennant d'espagne spétialisé dans la cuisine espagnole
+              bla bla</p>
+              <p>Retrouvez moi au chiringuito aux miles tout les jours bla bla</p>
+          </div>
+    </aricle>
     <style>
         div{
             border: solid 1px black;
             width:25%;
-            height: 100px;  
+            height: auto;  
 
         }
     </style>
